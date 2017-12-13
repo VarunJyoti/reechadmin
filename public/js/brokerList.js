e23517d71e70a6eb898f4c3346b304347e17aa13
Anil Kumar Goyal <Anil.Kumar.Goyal@basware.com>
2017-12-13 17:56:43 +0530

    server side search working without paging

--- public/js/brokerList.js
+++ public/js/brokerList.js
@@ -1,21 +1,47 @@
 $(document).ready(function () {
     //https://datatables.net/examples/data_sources/ajax.html
     $('#brokersList').DataTable({
-        "ajax": {url: "/listBroker", dataSrc: "data"},
-        "lengthMenu": [25, 50 ],
-        "scrollY": "550px",
-        "columns": [
-            {"data": "name"},
-            {"data": "position"},
-            {"data": "office"},
-            {"data": "extn"},
-            {"data": "start_date"},
-            {"data": "salary"}
+        ajax: {url: "/listBroker", dataSrc: "data"},
+        lengthMenu: [5, 25, 50 ],
+        scrollY: "550px",
+        scrollx: true,
+        columns: [
+            {data: "key"},
+            {data: "empl_name"},
+            {data: "licenseNumber"},
+            {data: "emp_email"},
+            {data: "city"},
+            {data: "state"},
+            {data: "county_name"},
+            {data: "agt_name"}
         ],
-        //"processing": true,
-        //"serverSide": true,
-        "ordering": false,
-        "info": true
+        processing: true,
+        serverSide: true,
+        ordering: false,
+        paging: true,
+        info: true,
+        searching : false,
     });
 
+    var icon = $('<i>').addClass('fa fa-search');
+    var lbl = $('<label>');
+    lbl.append($('<input>').prop( "type", "search").attr("id", "brokersList_search").attr("aria-controls","brokersList").addClass("input-sm"));
+    lbl.append($('<button>').prop("id","searchData").addClass("btn btn-primary").append(icon));
+
+    var customSearch = $('<div/>').addClass("dataTables_filter").prop("id","brokersList_filter");
+    customSearch.insertAfter(".dataTables_length")
+    customSearch.append(lbl);
+
+    $('#searchData').click(function () {
+        var v = $("#brokersList_search").val();
+        var table = $('#brokersList').DataTable();
+        table.search(v, false, true).draw();
+    });
+
+    /*
+    *1. get data from fb()
+    *2. 15k 50... query?
+    *3. skip(pageSize).select(pageSize)
+    */
 });
+
--- routes/listBroker.js
+++ routes/listBroker.js
@@ -1,11 +1,48 @@
 var express = require('express');
 var brokersJson = require('../brokers.json');
 const router = express.Router();
-
-router.get('/listBroker', function(req, res, next) {
+const admin = require('firebase-admin');
+const reechWebAccount = require("../reechappweb.json");
+admin.initializeApp({
+    credential: admin.credential.cert(reechWebAccount),
+    databaseURL: "https://reechappweb-bc8bd.firebaseio.com"
+});
+const db = admin.database();
+var last ;
+router.get('/listBroker', function (req, res, next) {
     var license = req.params.id || "";
+    var startIdx = parseInt(req.query.start);
+    var len = parseInt(req.query.length);
 
-    res.json(brokersJson);
+    var resJson = {}
+    resJson.recordsTotal = brokersJson.data.length;
+    resJson.recordsFiltered = brokersJson.data.length;
+    resJson.data = brokersJson.data.slice(startIdx, startIdx + len)
+    //res.json(resJson);
+    
+    var ref = db.ref('agentDB').orderByChild("lic_number");
+    if (startIdx == 0) {
+        ref.limitToFirst(len);
+    } else {
+        ref.startAt(last).limitToFirst(len);
+    }
+    ref.on("value", function(s) {
+        var obj = s.val();
+
+        var resObjs = [];
+        for (var objkey in obj) {
+            dbObj = obj[objkey]
+            var resObj = {"key":objkey};
+            last = dbObj["lic_number"];
+            for (var prop in dbObj) {
+                {
+                    resObj[prop] = dbObj[prop]
+                }
+            }
+            resObjs.push(resObj);
+        }
+        res.json({"data":resObjs})
+    });
 });
 
 module.exports = router;
